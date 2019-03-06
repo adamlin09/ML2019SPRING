ab93752a5ae2b4f14cc2f092fcaa572cf97fe221
adam <b04505028@ntu.edu.tw>
2019-03-06 10:54:29 +0800

    hw1

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -11,8 +11,17 @@ test_y = []
 for i in testdata :
     if row % 18 == 0 :
         test_x.append([1])
+    elif row % 18 == 13 or row % 18 == 1 or row % 18 == 10:
+        row += 1
+        continue
     for j in range(9) :
-        test_x[row//18].append(0.0 if i[2+j] == 'NR' else float(i[2+j]))
+        if i[2+j] == 'NR' :
+            test_x[row//18].append(0.0)
+        else :
+            a = float(i[2+j])
+            if a < 0.0 :
+                a = -a
+            test_x[row//18].append(a)
     row += 1
 test_y = np.dot(test_x, w)
 
--- hw1/train.py
+++ hw1/train.py
@@ -1,4 +1,4 @@
-import csv, sys
+import csv, sys, math
 import numpy as np
 
 data = [[] for i in range(18)]
@@ -6,37 +6,50 @@ f = open('./ml2019spring-hw1/train.csv', encoding = 'Big5')
 d = csv.reader(f)
 index = 0
 for i in d :
-    if index == 0 :
-        pass
-    else :
-        for j in i[3:] :
-            data[(index-1)%18].append(0.0 if j == 'NR' else float(j))
-    index += 1
+	if index == 0 :
+		pass
+	else :
+		for j in i[3:] :
+			if j == 'NR' :
+				data[(index-1)%18].append(0.0)
+			else:
+				a = float(j)
+				if a < 0.0 :
+					a = -a
+				data[(index-1)%18].append(float(j))
+	index += 1
 f.close()
 
 train_x, train_y = [], []
 for i in range(12) :
-    for j in range(471):
-        train_x.append([])
-        for k in range(18) :
-            for l in range(9) :
-                train_x[471*i+j].append(data[k][480*i+j+l])
-        train_y.append(data[9][480*i+9+j])
+	for j in range(471):
+		train_x.append([])
+		for k in range(18) :
+			if k == 13 or k == 1 or k == 10:
+				continue
+			for l in range(9) :
+				train_x[471*i+j].append(data[k][480*i+j+l])
+		train_y.append(data[9][480*i+9+j])
 train_x = np.array(train_x)
 train_y = np.array(train_y)
 train_x = np.concatenate((np.ones((train_x.shape[0],1)),train_x), axis=1)
 
 #initial
 w = np.ones(train_x.shape[1])
-lr = 1
-iteration = 100000
+lr = 0.5
+iteration = 300000
 prev_grad = 0
 
 for i in range(iteration) :
-    y = np.dot(train_x, w)
-    loss = y - train_y
-    grad = 2*np.dot(train_x.T, loss)
-    prev_grad += grad**2
-    ada = np.sqrt(prev_grad)
-    w -= lr * grad / ada
+	y = np.dot(train_x, w)
+	loss = y - train_y
+	grad = 2*np.dot(train_x.T, loss)
+	prev_grad += (grad**2)
+	ada = np.sqrt(prev_grad)
+	w -= lr * grad / ada
+	if i % 1000 == 0 :
+		cost = np.sum(loss**2) / len(loss)
+		cost_a = math.sqrt(cost)
+		print(cost_a)
+
 np.save('model.npy', w)
