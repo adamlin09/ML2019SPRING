e2b20421f3da51745d1733c913ac2a86cc807ba1
adam <b04505028@ntu.edu.tw>
2019-05-23 11:39:48 +0800

    hw7

--- hw7/cluster.py
+++ hw7/cluster.py
@@ -17,7 +17,8 @@ from keras.layers import Dense, Input
 from keras.models import Model
 from keras import backend as K
 from sklearn.cluster import KMeans
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, KernelPCA, IncrementalPCA
+from sklearn.manifold import TSNE
 import pandas as pd
 
 start = time.time()
@@ -32,42 +33,42 @@ def load_data(path):
 
     imgs = []
     for i in range(len(f_names)):  # f_names为所有图片地址，list
-        img = io.imread(f_names[i])
-        img = img_as_float(img)
+        img = io.imread(f_names[i]).astype('float')
+        img /= 255.0
         imgs.append(img) # 把图片数组加到一个列表里面
         print('\r> Loading \'{}\''.format(f_names[i]), end="", flush=True)
     print('')
     imgs = np.array(imgs)
     return imgs
 
-input_img = Input(shape=(32,32,3))  # adapt this if using `channels_first` image data format
+# input_img = Input(shape=(32,32,3))  # adapt this if using `channels_first` image data format
 
-x = Conv2D(64, (3, 3), activation='relu', padding='same')(input_img)
-x = BatchNormalization()(x)
-x = MaxPooling2D((2, 2), padding='same')(x)
-x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-x = BatchNormalization()(x)
-x = MaxPooling2D((2, 2), padding='same')(x)
-x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
-x = BatchNormalization()(x)
-encoded = MaxPooling2D((2, 2), padding='same')(x)
+# x = Conv2D(64, (3, 3), activation='relu', padding='same')(input_img)
+# x = BatchNormalization()(x)
+# x = MaxPooling2D((2, 2), padding='same')(x)
+# x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+# x = BatchNormalization()(x)
+# x = MaxPooling2D((2, 2), padding='same')(x)
+# x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+# x = BatchNormalization()(x)
+# encoded = MaxPooling2D((2, 2), padding='same')(x)
 
-# at this point the representation is (4, 4, 8) i.e. 128-dimensional
+# # at this point the representation is (4, 4, 8) i.e. 128-dimensional
 
-x = Conv2D(16, (3, 3), activation='relu', padding='same')(encoded)
-x = BatchNormalization()(x)
-x = UpSampling2D((2, 2))(x)
-x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-x = BatchNormalization()(x)
-x = UpSampling2D((2, 2))(x)
-x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
-x = BatchNormalization()(x)
-x = UpSampling2D((2, 2))(x)
-decoded = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(x)
+# x = Conv2D(16, (3, 3), activation='relu', padding='same')(encoded)
+# x = BatchNormalization()(x)
+# x = UpSampling2D((2, 2))(x)
+# x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+# x = BatchNormalization()(x)
+# x = UpSampling2D((2, 2))(x)
+# x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
+# x = BatchNormalization()(x)
+# x = UpSampling2D((2, 2))(x)
+# decoded = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(x)
 
-autoencoder = Model(input_img, decoded)
-autoencoder.summary()
-autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+# autoencoder = Model(input_img, decoded)
+# autoencoder.summary()
+# autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
 
 # train_data = np.load('train_data.npy')
 train_data = load_data(sys.argv[1])
@@ -77,21 +78,23 @@ np.save('train_data.npy', train_data)
 # print(img_as_float(train_data[0])*255.0)
 # train_data /= 255.0
 
-autoencoder.fit(train_data, train_data,
-                epochs=50,
-                batch_size=128,
-                shuffle=True,
-                validation_split=0.1)
-encoder = Model(input_img, encoded)
-encoder.save('encoder.h5')
-autoencoder.save('autoencoder.h5')
-# encoder = load_model('encoder.h5')
+# autoencoder.fit(train_data, train_data,
+#                 epochs=50,
+#                 batch_size=128,
+#                 shuffle=True,
+#                 validation_split=0.1)
+# encoder = Model(input_img, encoded)
+# encoder.save('encoder.h5')
+# autoencoder.save('autoencoder.h5')
+encoder = load_model('encoder.h5')
 # autoencoder = load_model('autoencoder.h5')
 encoded_imgs = encoder.predict(train_data)
 print(encoded_imgs.shape)
 
 pca = PCA(n_components=200, copy=False, whiten=True, svd_solver='full')
 new_vec = pca.fit_transform(encoded_imgs.reshape(-1, 256))
+# tsne = TSNE(n_components=4, init='pca', random_state=0)
+# new_vec = tsne.fit_transform(encoded_imgs.reshape(-1, 256))
 
 # test = autoencoder.predict(train_data[6].reshape(-1, 32, 32, 3))
 # t = test*255.0
